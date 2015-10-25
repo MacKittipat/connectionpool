4c791f76175bbcedf6895dd68cdb0a775a39bd5e
Mac <mac@abctech-thailand.com>
2015-10-25 13:00:12 +0700

    Create 10 thread that send request

--- connapp/src/main/java/com/mackittipat/connectionpool/connapp/App.java
+++ connapp/src/main/java/com/mackittipat/connectionpool/connapp/App.java
@@ -3,10 +3,38 @@ package com.mackittipat.connectionpool.connapp;
 import org.apache.http.client.fluent.Request;
 
 import java.io.IOException;
+import java.util.stream.IntStream;
 
 public class App {
 
     public static void main(String[] args) throws IOException {
-        System.out.println(Request.Get("http://localhost:9000/connwebapp/save").execute().returnContent());
+
+
+//        Runnable runnable = () -> {
+//            IntStream.range(1, 1000).forEach((num) -> {
+//                try {
+//                    System.out.println(Request.Get("http://localhost:9000/connwebapp/save").execute().returnContent());
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            });
+//        };
+//        new Thread(runnable).start();
+
+
+
+        IntStream.range(1, 10).forEach((num) -> {
+            Runnable runnable = () -> {
+                IntStream.range(1, 1000).forEach((num2) -> {
+                    try {
+                        System.out.println(num + " >> " + Request.Get("http://localhost:9000/connwebapp/save").execute().returnContent());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+            };
+            new Thread(runnable).start();
+        });
+
     }
 }
